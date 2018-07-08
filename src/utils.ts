fb9121cfe00b5c35b3a8c4b7879505ddcc9efd34
johnny <johnny@localmomentum.net>
2018-07-08 08:01:30 -0400

    add isPackaged to utils until it is in an official electron release

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,15 @@
+import * as path from 'path';
+
+// is a property on app in https://github.com/electron/electron/commit/daf75dd3755c994fdfba3f70686e75f4e4c4a2a2#diff-6ba4c23225436343131188beec8d2370
+// but is not in the current 2.x releases.
+export const isPackaged = () => {
+  const execFile = path.basename(process.execPath).toLowerCase();
+  if (process.platform === 'win32') {
+    return execFile !== 'electron.exe';
+  }
+  return execFile !== 'electron';
+};
+
 export const isDev = () => {
   if (process.env.ELECTRON_IS_DEV === undefined) {
     // /[\\/]electron/.test(process.execPath);
