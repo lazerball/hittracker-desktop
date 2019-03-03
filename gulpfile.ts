e3c51f00010ad1fb1e26fbf639b7cbf2b2e842dc
Johnny Robeson <johnny@localmomentum.net>
2019-03-03 08:49:54 -0500

    bump hittracker to 0.3.24

--- gulpfile.ts
+++ gulpfile.ts
@@ -91,7 +91,7 @@ const fetchHitTracker = async (unpackDir: string, platform: string) => {
     console.log('Not downloading HitTracker since we already have it.');
     return;
   }
-  const hitTrackerVersion = '0.3.20';
+  const hitTrackerVersion = '0.3.24';
   const file = `HitTracker-electron-${platform}-${hitTrackerVersion}.tar.bz2`;
   const url = `https://github.com/lazerball/HitTracker/releases/download/${hitTrackerVersion}/${file}`;
   try {
